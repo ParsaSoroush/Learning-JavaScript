4a883e8835152bee985185be67a17d803f7df000
Parsa Soroush <parsakingkp@gmail.com>
2025-03-16 11:12:43 +0330

    [200~JavaScript provides 8 mathematical constants that can be accessed as Math properties:

---
+++ 21-math-propeties.js
@@ -0,0 +1,10 @@
+let e = Math.E        // returns Euler's number
+let pi = Math.PI       // returns PI
+let sqrt2 = Math.SQRT2    // returns the square root of 2
+let sqrt1_2 = Math.SQRT1_2  // returns the square root of 1/2
+let ln2 = Math.LN2      // returns the natural logarithm of 2
+let ln10 = Math.LN10     // returns the natural logarithm of 10
+let log2e = Math.LOG2E    // returns base 2 logarithm of E
+let log10e = Math.LOG10E   // returns base 10 logarithm of E
+
+console.log(e, pi, sqrt2, sqrt1_2, ln2, ln10, log2e, log10e);
